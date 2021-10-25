d5d7e17d5c0c7aa091ac13c3a9b9137dcf41b5d7
Simran KC <simrankc@Simrans-MacBook-Pro.local>
2021-10-25 06:22:44 +0545

    Spell correction added

--- main.py
+++ main.py
@@ -2,10 +2,39 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import urllib.parse, http.client, urllib.request, urllib.error, json
+import requests
+import json
+from string import punctuation
 
 textAnalyticsEndpoint = 'textprocessinginstance.cognitiveservices.azure.com'
 textAnalyticsKey = st.secrets["textAnalyticsKey"]
 
+def spell_correct(data):
+    api_key = st.secrets["spellCheckKey"]
+    endpoint = "https://api.bing.microsoft.com/v7.0/SpellCheck"
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Ocp-Apim-Subscription-Key': api_key,
+    }
+
+    text = {'text': data}
+    params = {
+    'mkt':'en-us',
+    'mode':'proof'
+    }
+    response = requests.post(endpoint, headers=headers, params=params, data=text)
+
+    json_response = response.json()
+
+    for token in json_response['flaggedTokens']:
+        data = data.replace(token['token'], token['suggestions'][0]['suggestion'])
+
+
+    if (json_response['flaggedTokens']!=[]):
+        st.write("You probably meant: "+ data)
+   
+    return data
 
 def key_extraction(data):
     headers = {
@@ -31,14 +60,13 @@ def key_extraction(data):
         data = json.loads(jsonData)
 
         document = data['documents'][0]
-        result = " ".join(x for x in document['keyPhrases'])
+        result = [x for x in document['keyPhrases']]
         conn.close()
+        return result
 
     except Exception as ex:
         print(ex)
-        return ""
 
-    return result
 
 
 def sentiment_analyze(data):
@@ -64,51 +92,60 @@ def sentiment_analyze(data):
         response = conn.getresponse()
         jsonData = response.read().decode('UTF-8') 
         data = json.loads(jsonData)
+
         document = data['documents'][0]
-        result = document['sentiment']
+        # result = document['sentiment']
         conn.close()
+        return document
 
     except Exception as ex:
         print(ex)
-        return ""
-
-    return result
 
 
-from string import punctuation
+
 
 
 def clean (data):
-    cleaned_data = ''.join(c for c in data if c==" " or (c.isalpha() and c!=" ")).lower()
-    return cleaned_data
+    try:
+        data = spell_correct(data)
+        data = ''.join(c for c in data if c==" " or (c.isalpha() and c!=" ")).lower()
+        return data
+
+    except Exception as ex:
+        print(ex)
 
 
-
-st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_sidebar_state='auto', menu_items=None)
 st.title('Sentiment Analysis & Key Phrases Extraction')
-
 st.header("Enter the text below! ")
 data = st.text_area("", "")
-
-data = clean(data)
 
 operation = st.selectbox("", ('Sentiment Analysis', 'Key Phrase Extraction', 'Whole analysis'))
 
 if st.button('Perform'):
+    data = clean(data)
 
     with st.spinner("Computing Results"):
+
         if operation == 'Sentiment Analysis' or operation == "Whole analysis":
             result = sentiment_analyze(data)
-            if result=="negative":
+            print(result)
+
+
+
+
+            if result['sentiment']=="negative":
               emoji =":pensive:"
-            if result=="neutral":
+            if result['sentiment']=="neutral":
               emoji =":neutral_face:"
-            if result=="positive":
+            if result['sentiment']=="positive":
               emoji =":grin:"
-            st.write(emoji + "  "+ result + " statement! ")
+            st.write(emoji + "  "+ result['sentiment'] + " statement! ")
 
 
         if operation == 'Key Phrase Extraction' or operation == "Whole analysis":
             result = key_extraction(data)
-            st.write(result)
+            
+
+
+            
       
